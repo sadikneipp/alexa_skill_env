5a2f1dfaa915f309dc2d09de5b794628efd5b879
claudiacdlira <claudiacdlira@gmail.com>
2018-11-17 23:37:07 +0000

    added other intent handlers

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,8 @@
 # the implementation of handler classes approach in skill builder.
 import logging
 
+from random import randint
+
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
@@ -13,25 +15,11 @@ from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 
-import requests
-
 sb = SkillBuilder()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-URL = 'http://35.197.194.231:5000/authenticate/'
-headers = "Content-Type: application/json"
-
-def check_auth(json):
-    r = requests.post(URL,
-                  json = json, #{"source":"mary","target":"john", "value": 100},
-                  headers = {"Content-Type": "application/json"})
-    ans = r.json()
-    if ans['auth'] == 1:
-        return True
-    
-    return False
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
@@ -57,42 +45,68 @@ class HelloWorldIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
+        #cash = handler_input["slots"]
         slots = handler_input.request_envelope.request.intent.slots
+        #request_envelope.request.intent.slots
         fact_number = int(slots["cash"].value)
-        fact_name = str(slots["person"].value).lower()
-        
-        auth = check_auth({"source": "mary",
-                    "target": fact_name,
-                    "value":fact_number})
-        
-    
-        if fact_number <= 10 and auth:
+        fact_name = str(slots["person"].value)
+
+
+        if fact_number <= 10:
             speech_text = "The payment of "  + str(fact_number) + " pounds to " + fact_name + " was done succesfully!"
-        elif fact_number > 10:
-            speech_text = "Uh thats too much! " + str(fact_number)
-        elif not auth:
-            speech_text = "Payment unauthorized!" + str(fact_number)
+        else:
+            speech_text = "Please authorize with the!"
 
 
         handler_input.response_builder.speak(speech_text).set_card(
             SimpleCard("Hello World", speech_text)).set_should_end_session(
-            True)
+            False)
         return handler_input.response_builder.response
 
 
-class HelpIntentHandler(AbstractRequestHandler):
+class CreditScoreIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+        return is_intent_name("CreditScoreIntent")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speech_text = "You can say hello to me!"
+        speech_text = "Your credit score is " + str(randint(0, 100)) + "."
 
-        handler_input.response_builder.speak(speech_text).ask(
-            speech_text).set_card(SimpleCard(
-                "Hello World", speech_text))
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False)
+        return handler_input.response_builder.response
+
+class BalanceIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("BalanceIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Your balance is £" + str(randint(1200, 3000)) +",00."
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False)
+        return handler_input.response_builder.response
+
+class RewardPointsIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("RewardPointsIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "You have "+ str(randint(10000, 60000)) + " reward points!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False)
         return handler_input.response_builder.response
 
 
@@ -162,7 +176,9 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(HelloWorldIntentHandler())
-sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CreditScoreIntentHandler())
+sb.add_request_handler(BalanceIntentHandler())
+sb.add_request_handler(RewardPointsIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(FallbackIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
